8f8b793f9405d32d09d5d9079c72603fb3f90f58
Muhammad Jahangir Zafar <muhammad.zafar@nexusgroup.com>
2024-09-14 13:48:16 +0200

    Initial Commit

---
+++ Trooute/TroouteApp.swift
@@ -0,0 +1,17 @@
+//
+//  TroouteApp.swift
+//  Trooute
+//
+//  Created by Muhammad Zafar on 2024-09-14.
+//
+
+import SwiftUI
+
+@main
+struct TroouteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
